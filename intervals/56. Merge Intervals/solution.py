4f8f876753e8f81b8a1eafba33e4ab175d3d3228
Steve Flores <stevewflores43@gmail.com>
2023-11-16 00:51:49 -0800

    56. Merge Intervals

---
+++ intervals/56. Merge Intervals/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        res = [intervals[0]]
+
+        for start, end in intervals:
+            prev_start, prev_end = res[-1]
+            if prev_end >= start:
+                res[-1][1] = max(prev_end,end)
+            else:
+                res.append([start,end])
+        return res
